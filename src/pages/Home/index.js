7cd642b77448dd627bd0d98166a5374bd2db691b
GeovaneSilva <silvafranco71@gmail.com>
2025-09-05 19:32:58 -0300

    adicionando arquivos

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,7 +3,7 @@ import { Text, View, TouchableOpacity } from "react-native";
 
 import styles from "./styles";
 
-export function Login() {
+export function Home() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Olá, React Native! 🚀</Text>
